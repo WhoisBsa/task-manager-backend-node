e207663444bb7d894749b0e6d1ce3e175d66ea39
Matheus Barbosa <mbscbsjbs@gmail.com>
2024-06-05 22:18:32 -0300

    ft: creating server

---
+++ server.js
@@ -0,0 +1,11 @@
+import 'dotenv';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env?.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server now listen on port ${PORT}`);
+});
